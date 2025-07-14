ee26ef94b69c4720d3b4d0fd9380e57b3a8d6a14
semdner <mxprogramming@protonmail.com>
2025-07-14 18:49:25 +0200

    Add help page

--- recon.sh
+++ recon.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+help() {
+    echo -e "recon.sh: bash recon.sh [-i][-t][-h] \n"
+    echo "   OPTIONS:"
+    echo "     -i      file with domains to scan"
+    echo "     -t      specify which tool to run"
+    echo "     -h      show help page (this)"
+}
+
 OUTPUT_DIR="output/"
+
+while getopts "h" flag
+do
+    case ${flag} in
+        h)
+            help
+            ;;
+        *)
+            ;;
+    esac
+done
 
 if [ ! -d "$OUTPUT_DIR" ]
 then
     echo "Creating output directory $OUTPUT_DIR"
     mkdir $OUTPUT_DIR
 fi
+
+
+
