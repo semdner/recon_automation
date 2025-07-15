0235e0d42d1f485e7b3fb872b1f1180c37b06630
semdner <mxprogramming@protonmail.com>
2025-07-15 16:32:45 +0200

    Add crtsh, gh-subs and dirsearch scan

--- recon.sh
+++ recon.sh
@@ -2,12 +2,16 @@
 
 source ./helper.lib
 source ./scan.lib
+source ./.env
 
 # default output directory
 OUTPUT_DIR="output/"
 
 # path to used tools
 PATH_TO_SUBLIST3R="/home/max/infosec/common/tools/Sublist3r/"
+PATH_TO_CRTSH="/home/max/infosec/common/tools/crt.sh"
+PATH_TO_DIRSEARCH="/home/max/infosec/common/tools/dirsearch"
+PATH_TO_GITHUB_SUBDOMAINS="/home/max/go/bin/github-subdomains"
 
 # get values from flags
 while getopts ":f:d:t:h" opt
